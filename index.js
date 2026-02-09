d35a58cc4dfe472b325b68ef9f2e60afa72e0224
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-02-09 21:53:22 +0530

    update

--- index.js
+++ index.js
@@ -33,9 +33,24 @@ app.get('/files/:filename', (req, res) => {
         res.render('show', { filename: filename, content: data});
     });
 });
-//         username: username
-//     });
-// });
+app.get('/edit/:filename', (req, res) => {
+    const filename = req.params.filename;
+    fs.readFile(`./files/${filename}`, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+        res.render('edit', { filename: filename, content: data});
+    });
+});
+app.post('/edit', (req, res) => {
+    const { details, newDetails } = req.body;
+    fs.rename(`./files/${details}`, `./files/${newDetails.split(" ").join('')}.txt`, (err) => {
+        if (err) {
+            return res.status(500).send('Error renaming file');
+        }
+        res.redirect('/');
+    });
+});
 
 
 app.listen(3000, () => {
